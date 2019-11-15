c1e23e4fda5a7d5e5ac1a97ab38e7c5609a42db9
Enes Özçelik <36846516+mnsozcelik@users.noreply.github.com>
2019-11-15 15:33:05 +0300

    v1.3
    General design configuration

--- Escape Game/Assets/Scripts/finishGate.cs
+++ Escape Game/Assets/Scripts/finishGate.cs
@@ -2,37 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class openGate : MonoBehaviour
+public class finishGate : MonoBehaviour
 {
-
     public Animation anim;
     public GameObject gate;
     // Start is called before the first frame update
     void Start()
     {
-        gate = GameObject.Find("gate");
+        gate = GameObject.Find("finishGate");
         anim = gate.GetComponent<Animation>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name== "Character")
-        { 
+        if (other.gameObject.name == "Character")
+        {
             //gatetrans.position = gatetrans.position + new Vector3(x, 0, 0);
             //Destroy(gate);
             anim.Play("openGate");
         }
-    }
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.name == "Character")
-        {
-            anim.Play("closeGate");
-        }   
     }
 }
