43035e1f3e124541c5cf010428f5c5b6b5341012
Enes Özçelik <36846516+mnsozcelik@users.noreply.github.com>
2019-11-18 00:02:25 +0300

    Create UI System
    Created Main Menu, Level Menu and Scene Switching System.

---
+++ Escape Game/Assets/Scripts/menuAnim.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuAnim : MonoBehaviour
+{
+    public float speed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 10f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate (Time.deltaTime * speed, Time.deltaTime * speed, Time.deltaTime * speed);
+    }
+    
+}
---
+++ Escape Game/Assets/Scripts/menuScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menuScript : MonoBehaviour
+{
+   
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("levelsScene");
+    }
+    public void InformationButton()
+    {
+        SceneManager.LoadScene("informationScene");
+    }
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+    public void L1()
+    {
+        SceneManager.LoadScene("1");
+    }
+    public void L2()
+    {
+        SceneManager.LoadScene("2");
+    }
+    public void L3()
+    {
+        SceneManager.LoadScene("3");
+    }
+    public void L4()
+    {
+        SceneManager.LoadScene("4");
+    }
+    public void L5()
+    {
+        SceneManager.LoadScene("5");
+    }
+    public void L6()
+    {
+        SceneManager.LoadScene("6");
+    }
+    public void L7()
+    {
+        SceneManager.LoadScene("7");
+    }
+    public void L8()
+    {
+        SceneManager.LoadScene("8");
+    }
+    public void L9()
+    {
+        SceneManager.LoadScene("9");
+    }
+    public void L10()
+    {
+        SceneManager.LoadScene("10");
+    }
+}
