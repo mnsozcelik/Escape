38b9beff3016986562e13185effa562ab752ffac
Enes Özçelik <36846516+mnsozcelik@users.noreply.github.com>
2019-12-07 23:57:54 +0300

    Pause menu and game timer uı added.

--- Escape Game/Assets/Scripts/menuScript2.cs
+++ Escape Game/Assets/Scripts/menuScript2.cs
@@ -1,39 +1,93 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class menuScript2 : MonoBehaviour
+public class menuScript2 : MonoBehaviour 
 {
-    // Start is called before the first frame update
-    GameObject pauseMenu;
-    GameObject pauseButton;
-    GameObject playButton;
+    public GameObject pauseMenu;
+    public GameObject pauseButton;
+    public GameObject playButton;
+    public GameObject handle;
+    public GameObject fixedJoystick;
+    public GameObject panel;
+    public GameObject gameOverMenu;
+    public 
+
+    bool isplay = false;
     void Start()
     {
-        pauseButton = GameObject.Find("PauseButton");
-        playButton = GameObject.Find("PlayButton");
-        pauseMenu = GameObject.Find("PauseMenu");
+        //pauseButton = GameObject.Find("PauseButton");
+        //playButton = GameObject.Find("PlayButton");
+        //pauseMenu = GameObject.Find("PauseMenu");
         pauseButton.SetActive(true);
         playButton.SetActive(false);
         pauseMenu.SetActive(false);
+        gameOverMenu.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        /*//timer timer = new timer();
+        //Debug.Log(timer.isOver);
+        if (timer.isOver == true)
+        {
+            isplay = false;
+            if (isplay == false)
+            {
+                playButton.SetActive(true);
+                pauseMenu.SetActive(true);
+                pauseButton.SetActive(false);
+                Time.timeScale = 0.0f;
+                handle.SetActive(false);
+                panel.SetActive(false);
+                fixedJoystick.SetActive(false);
+                gameOverMenu.SetActive(false);
+            }
+        }*/
+       
     }
 
     public void gamepause()
     {
-        playButton.SetActive(true);
-        pauseMenu.SetActive(true);
-        pauseButton.SetActive(false);
+        isplay = false;
+        if (isplay == false)
+        {
+            playButton.SetActive(true);
+            pauseMenu.SetActive(true);
+            pauseButton.SetActive(false);
+            Time.timeScale = 0.0f;
+            handle.SetActive(false);
+            panel.SetActive(false);
+            fixedJoystick.SetActive(false);
+            gameOverMenu.SetActive(false);
+        }
     }
     public void gameplay()
     {
-        playButton.SetActive(false);
-        pauseMenu.SetActive(false);
-        pauseButton.SetActive(true);
+        isplay = true;
+        if (isplay == true)
+        {
+            playButton.SetActive(false);
+            pauseMenu.SetActive(false);
+            pauseButton.SetActive(true);
+            Time.timeScale = 1.0f;
+            handle.SetActive(true);
+            panel.SetActive(true);
+            fixedJoystick.SetActive(true);
+            gameOverMenu.SetActive(false);
+        }
+    }
+
+    public void replay()
+    {
+        Time.timeScale = 1.0f;
+        string activescene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(activescene);
+    }
+    public void mainMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("menu");
     }
 }
---
+++ Escape Game/Assets/Scripts/timer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class timer : MonoBehaviour
+{
+    public Text timerText;
+    private float startTime;
+    public float idealTime;
+    public GameObject Timer;
+    public Image TimerImage;
+    public bool isOver=false;
+    public Color orange;
+    void Start()
+    {
+        startTime = Time.time;
+        idealTime = 4.0f;
+        TimerImage = Timer.GetComponent<Image>();
+        TimerImage.color = UnityEngine.Color.green;
+        orange=new Color(1f, 0.6f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float t = idealTime + (startTime - Time.time);
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString("f0");
+        timerText.text = minutes + ":" + seconds;
+        float percentIdealTiming = (Time.time - startTime) / idealTime;
+        //Debug.Log(percentIdealTiming);
+        
+        if (percentIdealTiming > 0.25)
+        {
+            TimerImage.color = UnityEngine.Color.yellow;
+        }
+        if (percentIdealTiming > 0.50)
+        {
+            TimerImage.color = orange;
+        }
+        if (percentIdealTiming > 0.75)
+        {
+            TimerImage.color = UnityEngine.Color.red;
+        }
+        if(percentIdealTiming >= 1)
+        {
+            isOver = true;
+        }
+    }
+}
