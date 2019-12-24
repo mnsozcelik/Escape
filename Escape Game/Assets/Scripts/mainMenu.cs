b29c4fad722d8f27b77fdd4f270df8f66d7cf0c5
Enes Özçelik <36846516+mnsozcelik@users.noreply.github.com>
2019-12-24 21:57:02 +0300

    Information Scene Level 6 7 8 Scene Created

---
+++ Escape Game/Assets/Scripts/mainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class mainMenu : MonoBehaviour
+{
+    public void mainmenu()
+    {
+        SceneManager.LoadScene("menu");
+    }
+    public void shareFacebook()
+    {
+        Application.OpenURL("https://www.facebook.com/mnsozcelik");
+        Debug.Log("facebook");
+    }
+
+    public void shareTwitter()
+    {
+        Application.OpenURL("https://www.twitter.com/hawkyshun");
+        Debug.Log("twitter");
+    }
+
+    public void shareInstagram()
+    {
+        Application.OpenURL("https://www.instagram.com/emreeskili3");
+        Debug.Log("instagram");
+    }
+}
